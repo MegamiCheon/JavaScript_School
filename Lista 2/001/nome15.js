03eaa324493688cad19b5448e4f0b69b6e12fbf8
Megami.ay <laizber35@gmail.com>
2021-11-16 11:22:27 -0300

    a

---
+++ Lista 2/001/nome15.js
@@ -0,0 +1,7 @@
+
+    var i,
+    Nome = prompt('Qual seu nome?');
+
+    for(i = 0; i <15; i++) {
+        document.write('Seu nome é' + Nome)
+    }
---
+++ Lista 2/002/nomexvez.js
@@ -0,0 +1,6 @@
+
+    var Nome = window.prompt('Qual seu nome?')
+    var Vezes = window.prompt('Quantas vezes quer repetir?')
+    var i
+
+    for (i=0; i < ; i++)
